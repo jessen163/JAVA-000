0e0fef07675690bb8726d4c8e440fd8c2f51687e
jessen <chenji@cvtt.com>
2020-10-18 20:39:23 +0800

    第2题代码调优

--- Week_01/src/HelloClassLoader.java
+++ Week_01/src/HelloClassLoader.java
@@ -6,32 +6,16 @@ import java.lang.reflect.Method;
  * 自定义ClassLoader，解析Hello.xlass
  */
 public class HelloClassLoader extends ClassLoader {
-    public static void main(String[] args) {
-        try {
-            Object cla = new HelloClassLoader().findClass("Hello").newInstance();
-            Method method = cla.getClass().getMethod("hello");
-            method.invoke(cla);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+            IllegalAccessException, InstantiationException, InvocationTargetException {
+        Object cla = new HelloClassLoader().findClass("Hello").newInstance();
+        Method method = cla.getClass().getMethod("hello");
+        method.invoke(cla);
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes = null;
-        try {
-            bytes = read(HelloClassLoader.class.getResource("Hello.xlass").getPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        byte[] bytes = read(HelloClassLoader.class.getResource("Hello.xlass").getPath());
         return defineClass(name, bytes, 0, bytes.length);
     }
 
@@ -39,20 +23,22 @@ public class HelloClassLoader extends ClassLoader {
      *读取文本数据并转换
      *
      */
-    public static byte[] read(String fileName) throws IOException {
-        InputStream is = new FileInputStream(new File(fileName));
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[is.available()];
-        int n = 0;
-        while ((n = is.read(buffer)) != -1) {
-            bos.write(buffer, 0, n);
+    public static byte[] read(String fileName) {
+        try (InputStream is = new FileInputStream(new File(fileName));
+             ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[is.available()];
+            int n = 0;
+            while ((n = is.read(buffer)) != -1) {
+                bos.write(buffer, 0, n);
+            }
+            byte[] bytes = bos.toByteArray();
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+            return bytes;
+        } catch (IOException ie) {
+            ie.printStackTrace();
         }
-        bos.close();
-        is.close();
-        byte[] bytes = bos.toByteArray();
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) (255 - bytes[i]);
-        }
-        return bytes;
+        return null;
     }
 }
